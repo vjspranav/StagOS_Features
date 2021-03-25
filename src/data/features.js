97b64c70bce82fef43483c0958cb309a0eb2081c
vjspranav <pranavasri@live.in>
2021-03-25 13:15:11 +0530

    Add more Feature GIFs

--- src/data/features.js
+++ src/data/features.js
@@ -5,4 +5,54 @@ export const features = [
     preview: "qs_panel_bg_use_new_tint",
     section: "QS",
   },
+  {
+    title: "Battery Bar",
+    summary: "Adds a battery bar in the Status Bar",
+    preview: "battery_bar_category",
+    section: "statusbar",
+  },
+  {
+    title: "Status Bar Clock Settings",
+    summary: "Allows customization of statusbar clock",
+    preview: "clock_settings",
+    section: "statusbar",
+  },
+  {
+    title: "Battery Icon Style",
+    summary: "Change the icon of the battery indicator in statusbar",
+    preview: "status_bar_battery_style",
+    section: "statusbar",
+  },
+  {
+    title: "Toggle to show battery percentage",
+    summary: "Option whether to show battery charging percentage on status bar",
+    preview: "status_bar_show_battery_percent",
+    section: "statusbar",
+  },
+  {
+    title: "Battery Percentage while charging",
+    summary:
+      "Allow showing battery percentage in statusbar only while charging",
+    preview: "status_bar_battery_style",
+    section: "statusbar",
+  },
+  {
+    title: "Network Traffic Indicator",
+    summary: "Showing incoming and outgoing network usage",
+    preview: "traffic",
+    section: "statusbar",
+  },
+  {
+    title: "Invert Navigation bar",
+    summary:
+      "Allow changing position of back and recents when using 3/2 Button navigation bar",
+    preview: "sysui_nav_bar_inverse",
+    section: "navigation",
+  },
+  {
+    title: "Power Menu Customisations",
+    summary: "Options available on power menu",
+    preview: "global_actions",
+    section: "power_menu",
+  },
 ];
